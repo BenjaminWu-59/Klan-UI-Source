f366ae8a2900f22a22e9c26ca78d13a7cbe66296
Benjamin <wuqiong3699@gmail.com>
2021-07-06 17:31:39 +0800

    基础路由搭建

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="topnav">
+  <div class="log"></div>
+  <div class="menu"></div>
+</div>
+  <div class="banner">
+    <h1>KanLan UI</h1>
+    <h2>一个不错的个人UI框架</h2>
+    <p class="actions">
+      <a>Github</a>
+      <a>开始</a>
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Home.vue'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
