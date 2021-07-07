08c560a88853a29bbf965bdb2ee31cfbd82e2151
Benjamin <wuqiong3699@gmail.com>
2021-07-07 19:07:09 +0800

    aside 滑动效果完成

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,23 +2,25 @@
   <div class="layout">
     <Topnav class="nav"/>
     <div class="content">
-      <aside v-if="menuVisible">
-        <h2>组件列表</h2>
-        <ol>
-          <li>
-            <router-link to="/doc/switch">Switch 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/button">Button 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/dialog">Dialog 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/tabs">Tabs 组件</router-link>
-          </li>
-        </ol>
-      </aside>
+      <transition name="slide">
+        <aside v-if="menuVisible">
+          <h2>组件列表</h2>
+          <ol>
+            <li>
+              <router-link to="/doc/switch">Switch 组件</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/button">Button 组件</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/dialog">Dialog 组件</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/tabs">Tabs 组件</router-link>
+            </li>
+          </ol>
+        </aside>
+      </transition>
       <main>
         <router-view/>
       </main>
@@ -46,9 +48,11 @@ export default {
   display: flex;
   flex-direction: column;
   height: 100vh;
+
   > .nav {
     flex-shrink: 0;
   }
+
   > .content {
     flex-grow: 1;
     padding-top: 60px;
@@ -58,38 +62,44 @@ export default {
     }
   }
 }
+
 .content {
   display: flex;
+
   > aside {
     flex-shrink: 0;
   }
+
   > main {
     flex-grow: 1;
-    padding: 16px;
+    padding: 30px;
     background: lightgreen;
   }
 }
- aside {
-   background: #d5eaea;
-   width: 150px;
-   padding:16px;
-   padding-top: 70px;
-   position: fixed;
-   top: 0;
-   left: 0;
-   height: 100%;
-   box-shadow: 5px 0 5px #707070;
-   > h2 {
-     margin-bottom: 4px;
-   }
 
-   > ol {
-     > li {
-       padding: 4px 0;
-     }
-   }
- }
- main {
+aside {
+  background: #d5eaea;
+  width: 150px;
+  padding: 80px 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  z-index: 1;
+  box-shadow: 5px 0 5px #707070;
+
+  > h2 {
+    margin-bottom: 4px;
+  }
+
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+
+main {
   overflow: auto;
 }
 </style>
