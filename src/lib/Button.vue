2c3d69e72e3365ff6de4f55d4b691d15e8ae52cf
Benjamin <wuqiong3699@gmail.com>
2021-07-08 18:06:21 +0800

    Button.vue 添加theme属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,12 +1,19 @@
 <template>
-<button>
+<button class="public-button"
+ :class="`theme-${theme}`"
+>
   <slot/>
 </button>
 </template>
 
 <script lang="ts">
 export default {
-  name: 'Button'
+  props:{
+    theme:{
+      type:String,
+      default:'button'
+    }
+  }
 };
 </script>
 
