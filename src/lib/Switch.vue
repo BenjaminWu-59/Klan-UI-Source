aaf1c6c3cb23f068eda93ca1e10e760e1ffbe129
Benjamin <wuqiong3699@gmail.com>
2021-07-08 11:39:07 +0800

    优化Switch之css代码

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -29,22 +29,30 @@ button{
   background: gray;
   border-radius: $h/2;
   position: relative;
-}
-span{
-  position: absolute;
-  top:2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2/2;
-  transition: left 0.2s;
+  > span{
+    position: absolute;
+    top:2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2/2;
+    transition: left 0.2s;
+  }
+  &.checked {
+    background: blue;
+    > span{
+      left: calc(100% - #{$h});}
+  }
+  &:focus { outline: none; }
+  &:active {
+    > span { width: $h2 + 4px; }
+  }
+  &.checked:active {
+    > span { width: $h2 + 4px; margin-left: -4px; }
+  }
 }
 
-button.checked {
-  background: blue;
-}
-button.checked > span{
-  left: calc(100% - #{$h});
-}
+
+
 </style>
