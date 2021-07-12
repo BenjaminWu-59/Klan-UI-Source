4f6e0df4dd3d5c45c8beefb56448468817224042
Benjamin <wuqiong3699@gmail.com>
2021-07-12 10:31:50 +0800

    支持level属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -16,14 +16,19 @@ export default {
     size:{
       type:String,
       default: 'normal',
-    }
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
   },
   setup(props){
-    const {theme,size} = props
+    const {theme,size,level} = props
     const classes = computed(()=>{
       return {
         [`kanlan-theme-${theme}`]: theme,
         [`kanlan-size-${size}`]: size,
+        [`kanlan-level-${level}`]:level,
       }
     })
     return {classes}
@@ -37,6 +42,7 @@ export default {
   $color: #333;
   $blue: #40a9ff;
   $radius: 4px;
+  $red: red;
   .public-button {
     box-sizing: border-box;
     height: $h;
@@ -51,6 +57,7 @@ export default {
     border: 1px solid $border-color;
     border-radius: $radius;
     box-shadow: 0 1px 0 fade-out(black, 0.95);
+    transition: background 250ms;
     & + & {
       margin-left: 8px;
     }
@@ -81,6 +88,7 @@ export default {
         background: darken(white, 5%);;
       }
     }
+
       &.kanlan-size-big{
         font-size: 24px;
         height: 48px;
@@ -91,5 +99,52 @@ export default {
         height: 20px;
         padding: 0 4px;
       }
+    &.kanlan-theme-button {
+      &.kanlan-level-main {
+        background: $blue;
+        color: white;
+        border-color: $blue;
+        &:hover,
+        &:focus {
+          background: darken($blue, 10%);
+          border-color: darken($blue, 10%);
+        }
+      }
+      &.kanlan-level-danger {
+        background: $red;
+        border-color: $red;
+        color: white;
+        &:hover,
+        &:focus {
+          background: darken($red, 10%);
+          border-color: darken($red, 10%);
+        }
+      }
+    }
+    &.kanlan-theme-link {
+      &.kanlan-level-danger {
+        color: $red;
+        &:hover,
+        &:focus {
+          color: darken($red, 10%);
+        }
+      }
+    }
+    &.kanlan-theme-text {
+      &.kanlan-level-main {
+        color: $blue;
+        &:hover,
+        &:focus {
+          color: darken($blue, 10%);
+        }
+      }
+      &.kanlan-level-danger {
+        color: $red;
+        &:hover,
+        &:focus {
+          color: darken($red, 10%);
+        }
+      }
+    }
   }
 </style>
