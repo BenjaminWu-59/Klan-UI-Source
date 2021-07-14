739ac1471ad1b92556051eba0300251bd945c747
Benjamin <wuqiong3699@gmail.com>
2021-07-14 11:17:45 +0800

    Tabs css1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,29 +1,59 @@
 <template>
-<div>
-  Tabs组件
-  <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-  <component v-for="(c,index) in defaults" :key="index" :is="c"/>
-</div>
+  <div>
+    <div class="kanlan-tabs">
+      <div class="kanlan-tabs-nav">
+        <div class="kanlan-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{ t }}</div>
+      </div>
+      <div class="kanlan-tabs-content">
+        <component class="kanlan-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue';
+
 export default {
-  setup(props,context){
-    const defaults = context.slots.default()
-    defaults.forEach((tag)=>{
-      if(tag.type !== Tab){
-        throw new Error('Tabs子标签必须是Tab')
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs子标签必须是Tab');
       }
-    })
-   const titles = defaults.map((tag)=>{
-      return  tag.props.title
-    })
-    return {defaults,titles}
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    return {defaults, titles};
   }
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.kanlan-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
 
 </style>
