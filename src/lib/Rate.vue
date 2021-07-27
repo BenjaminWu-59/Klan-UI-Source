61f121db281072c5343d2c1415d8ca5fc520eb40
Benjamin <wuqiong3699@gmail.com>
2021-07-27 21:51:08 +0800

    rate组件 基本变色逻辑实现2.0

--- src/lib/Rate.vue
+++ src/lib/Rate.vue
@@ -7,6 +7,8 @@
       <li @click="L1" ref="A"></li>
       <li @click="L2" ref="B"></li>
       <li @click="L3" ref="C"></li>
+      <li @click="L4" ref="D"></li>
+      <li @click="L5" ref="E"></li>
     </ol>
   </div>
 </div>
@@ -20,6 +22,8 @@ export default {
      const A = ref()
      const B = ref()
      const C = ref()
+     const D = ref()
+     const E = ref()
      const L1 = ()=>{
        if(A.value.style.background === ''){
          A.value.style.background = 'yellow'
@@ -28,6 +32,8 @@ export default {
          A.value.style.background = ''
          B.value.style.background = ''
          C.value.style.background = ''
+         D.value.style.background = ''
+         E.value.style.background = ''
        }
      }
      const L2 = ()=>{
@@ -37,11 +43,38 @@ export default {
          C.value.style.background = ''
        }else {
          C.value.style.background = ''
+         D.value.style.background = ''
+         E.value.style.background = ''
        }
      }
 
      const L3 = ()=>{
        if(C.value.style.background === ''){
+         C.value.style.background = 'yellow'
+         B.value.style.background = 'yellow'
+         A.value.style.background = 'yellow'
+         D.value.style.background = ''
+       }else {
+         D.value.style.background = ''
+         E.value.style.background = ''
+       }
+     }
+
+     const L4 = ()=>{
+       if(D.value.style.background === ''){
+         D.value.style.background = 'yellow'
+         C.value.style.background = 'yellow'
+         B.value.style.background = 'yellow'
+         A.value.style.background = 'yellow'
+       }else{
+         E.value.style.background = ''
+       }
+     }
+
+     const L5 = ()=>{
+       if(E.value.style.background === ''){
+         E.value.style.background = 'yellow'
+         D.value.style.background = 'yellow'
          C.value.style.background = 'yellow'
          B.value.style.background = 'yellow'
          A.value.style.background = 'yellow'
@@ -54,9 +87,13 @@ export default {
        A,
        B,
        C,
+       D,
+       E,
        L1,
        L2,
-       L3
+       L3,
+       L4,
+       L5
      }
    }
 };
